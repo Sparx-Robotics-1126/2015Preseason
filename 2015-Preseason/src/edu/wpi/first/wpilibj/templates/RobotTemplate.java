d4363ccd846428af086b34da5f5578c8ee104768
Michael-Edwards777 <Mikee777@rochester.rr.com>
2014-12-03 18:30:57 -0500

    I created 6 talons.

--- 2015-Preseason/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ 2015-Preseason/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -18,19 +18,35 @@ import edu.wpi.first.wpilibj.IterativeRobot;
  * directory.
  */
 public class RobotTemplate extends IterativeRobot {
+	private Talon FrightT1;
+	private Talon BrightT2;
+	private Talon RrightT3;
+	private Talon FleftT4;
+	private Talon BleftT5;
+	private Talon RleftT6;
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
     public void robotInit() {
-
+    	FrightT1 = new Talon(1);
+    	BrightT2 = new Talon(1);
+    	RrightT3 = new Talon(1);
+    	FleftT4 = new Talon(1);
+    	BleftT5 = new Talon(1);
+    	RleftT6 = new Talon(1);
     }
 
     /**
      * This function is called periodically during autonomous
      */
     public void autonomousPeriodic() {
-
+    	FrightT1.set(-0.01);
+    	BrightT2.set(0.01);
+    	RrightT3.set(-0.01);
+    	FleftT4.set(0.01);
+    	BleftT5.set(-0.01);
+    	RleftT6.set(0.01);
     }
 
     /**
