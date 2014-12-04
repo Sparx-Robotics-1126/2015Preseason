f5353416a5b9d52e6722439ec7014ab4c6129d3b
Michael-Edwards777 <Mikee777@rochester.rr.com>
2014-12-03 19:17:23 -0500

    New controller, had to fix joystick parameter.

--- 2015-Preseason/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ 2015-Preseason/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -44,9 +44,9 @@ public class RobotTemplate extends IterativeRobot {
 	 */
 	public void autonomousPeriodic() {
 		while(true){
-			FrightT1.set(-joystick.getRawAxis(4));
-			BrightT2.set(joystick.getRawAxis(4));
-			RrightT3.set(-joystick.getRawAxis(4));
+			FrightT1.set(-joystick.getRawAxis(3));
+			BrightT2.set(joystick.getRawAxis(3));
+			RrightT3.set(-joystick.getRawAxis(3));
 			FleftT4.set(joystick.getRawAxis(2));
 			BleftT5.set(-joystick.getRawAxis(2));
 			RleftT6.set(joystick.getRawAxis(2));
